349e3d4509809022893ea8fe14ab436d2b7448ff
Stefan Härtel <haertel.dev@gmail.com>
2016-04-09 19:50:18 +0200

    Read properties file

--- src/main/groovy/de/eightbitboy/jenot/Jenot.groovy
+++ src/main/groovy/de/eightbitboy/jenot/Jenot.groovy
@@ -1,3 +1,11 @@
 package de.eightbitboy.jenot
 
 println 'Hello world!'
+
+Properties properties = new Properties()
+File propertiesFile = new File('jenot.properties')
+propertiesFile.withInputStream {
+    properties.load(it)
+}
+
+println properties
