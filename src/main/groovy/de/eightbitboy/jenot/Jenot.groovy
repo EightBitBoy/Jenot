246a06e0b459ec6e42762b5dba1ae9ca6349a650
Stefan Härtel <haertel.dev@gmail.com>
2016-04-10 08:59:00 +0200

    Create a main class, play around with the jenkins api

--- src/main/groovy/de/eightbitboy/jenot/Jenot.groovy
+++ src/main/groovy/de/eightbitboy/jenot/Jenot.groovy
@@ -1,13 +1,30 @@
 package de.eightbitboy.jenot
 
 import com.offbytwo.jenkins.JenkinsServer
+import com.offbytwo.jenkins.model.Build
+import com.offbytwo.jenkins.model.JobWithDetails
 
-println 'Hello world!'
 
-Properties properties = new Properties()
-File propertiesFile = new File('jenot.properties')
-propertiesFile.withInputStream {
-    properties.load(it)
+class Jenot {
+    public static void main(String... args) {
+        println 'Hello world!'
+
+        Properties properties = new Properties()
+        File propertiesFile = new File('jenot.properties')
+        propertiesFile.withInputStream {
+            properties.load(it)
+        }
+
+        println properties
+
+        JenkinsServer jenkins = new JenkinsServer(new URI(properties.server as String), properties.user as String, properties.password as String)
+        JobWithDetails job = jenkins.getJob(properties.job as String)
+
+        println job
+
+        List<Build> builds = job.getBuilds()
+        builds.each{
+            println it.details().getDuration()
+        }
+    }
 }
-
-println properties
