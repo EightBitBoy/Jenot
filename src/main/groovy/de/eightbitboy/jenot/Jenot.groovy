793910f133c4abb0834b25c477e2f0f1cebc4062
Stefan Härtel <haertel.dev@gmail.com>
2016-04-10 09:53:45 +0200

    Add tray icon and tray ui

--- src/main/groovy/de/eightbitboy/jenot/Jenot.groovy
+++ src/main/groovy/de/eightbitboy/jenot/Jenot.groovy
@@ -4,6 +4,14 @@ import com.offbytwo.jenkins.JenkinsServer
 import com.offbytwo.jenkins.model.Build
 import com.offbytwo.jenkins.model.JobWithDetails
 
+import javax.swing.ImageIcon
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.UIManager
+import java.awt.MenuItem
+import java.awt.PopupMenu
+import java.awt.SystemTray
+import java.awt.TrayIcon
 
 class Jenot {
     public static void main(String... args) {
@@ -17,6 +25,7 @@ class Jenot {
 
         println properties
 
+        /*
         JenkinsServer jenkins = new JenkinsServer(new URI(properties.server as String), properties.user as String, properties.password as String)
         JobWithDetails job = jenkins.getJob(properties.job as String)
 
@@ -26,5 +35,37 @@ class Jenot {
         builds.each{
             println it.details().getDuration()
         }
+        */
+
+
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+        /*
+        JFrame frame = new JFrame("HelloWorldSwing");
+
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        frame.pack();
+        frame.setVisible(true);
+        */
+
+        if (!SystemTray.isSupported()) {
+            println "No system tray!"
+        }
+
+        SystemTray tray = SystemTray.getSystemTray();
+
+        TrayIcon trayIcon = new TrayIcon(new ImageIcon("tray.png", "Icon").getImage())
+
+        PopupMenu popup = new PopupMenu();
+
+        MenuItem aboutItem = new MenuItem("About");
+
+        popup.add(aboutItem)
+
+        trayIcon.setPopupMenu(popup)
+
+        tray.add(trayIcon)
     }
 }
