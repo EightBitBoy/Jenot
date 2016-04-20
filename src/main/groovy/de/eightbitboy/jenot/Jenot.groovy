ad8da1e3b35c7d971a0390a2eefed564bdbbab02
Stefan Härtel <haertel.dev@gmail.com>
2016-04-20 21:11:04 +0200

    Exclude some code to start hacking

--- src/main/groovy/de/eightbitboy/jenot/Jenot.groovy
+++ src/main/groovy/de/eightbitboy/jenot/Jenot.groovy
@@ -1,20 +1,5 @@
 package de.eightbitboy.jenot
 
-import com.offbytwo.jenkins.JenkinsServer
-import com.offbytwo.jenkins.model.Build
-import com.offbytwo.jenkins.model.JobWithDetails
-import de.eightbitboy.jenot.ui.Notification
-import de.eightbitboy.jenot.ui.Ui
-
-import javax.swing.ImageIcon
-import javax.swing.JFrame
-import javax.swing.JLabel
-import javax.swing.UIManager
-import java.awt.MenuItem
-import java.awt.PopupMenu
-import java.awt.SystemTray
-import java.awt.TrayIcon
-
 import com.typesafe.config.Config
 
 class Jenot {
@@ -27,10 +12,12 @@ class Jenot {
             properties.load(it)
         }
 
+        println 'properties:'
         println properties
 
         Config config = Configuration.getInstance().getConfig()
 
+        println 'config:'
         println config
         /*
         JenkinsServer jenkins = new JenkinsServer(new URI(properties.server as String), properties.user as String, properties.password as String)
@@ -44,7 +31,7 @@ class Jenot {
         }
         */
 
-        new Ui()
-        new Notification("Hello world!")
+        //new Ui()
+        //new Notification("Hello world!")
     }
 }
