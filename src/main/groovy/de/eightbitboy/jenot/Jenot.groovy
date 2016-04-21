e79363ad824240fc310e8094c9d2950ffa652b77
Stefan Härtel <haertel.dev@gmail.com>
2016-04-21 16:09:12 +0200

    Play with Swing, find out about layout

--- src/main/groovy/de/eightbitboy/jenot/Jenot.groovy
+++ src/main/groovy/de/eightbitboy/jenot/Jenot.groovy
@@ -3,6 +3,11 @@ package de.eightbitboy.jenot
 import com.typesafe.config.Config
 import de.eightbitboy.jenot.ui.Ui
 
+import javax.swing.BoxLayout
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.JPanel
+
 import static com.google.common.truth.Truth.assertThat
 
 class Jenot {
@@ -26,7 +31,27 @@ class Jenot {
         assertThat(properties['server']).named('server').isNotNull()
 
         Jenkins jenkins = new Jenkins(properties['server'] as String)
-        new Ui(jenkins)
+
+
+        JFrame main = new JFrame('Main')
+        main.setSize(400, 400)
+
+        JPanel container = new JPanel()
+        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS))
+
+        container.add(new JLabel("Hello"))
+        container.add(new JLabel("Again"))
+
+        main.add(container)
+        main.setVisible(true)
+
+        //new Ui(jenkins)
         //new Notification("Hello world!")
+    }
+}
+
+class MyComponent{
+    MyComponent(){
+
     }
 }
