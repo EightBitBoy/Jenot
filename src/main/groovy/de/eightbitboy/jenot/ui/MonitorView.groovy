e88dcb6da051742ccea7410626e2ac35781896b3
Stefan Härtel <haertel.dev@gmail.com>
2016-04-21 20:42:51 +0200

    Small refactoring

--- src/main/groovy/de/eightbitboy/jenot/ui/MonitorView.groovy
+++ src/main/groovy/de/eightbitboy/jenot/ui/MonitorView.groovy
@@ -9,33 +9,30 @@ import java.util.List
 class MonitorView extends JFrame {
     private List<JobStatusView> statuses = []
 
-    JPanel content
+    private JPanel content
 
     MonitorView(Map<String, JobWithDetails> jobs) {
         super('MonitorView')
 
-        this.content = new JPanel(new GridLayout(5, 1));
-
-        jobs.each { name, job ->
-            println 'Adding job' + name
-
-            def status = new JobStatusView(job)
-            statuses.add(status)
-            addStatus(status)
-        }
-
-
-        setSize(600, 400)
+        this.content = new JPanel(new GridLayout(5, 1))
         add(this.content)
+        addStatusViews(jobs)
         pack()
         setVisible(true)
     }
 
-    private void addStatus(JobStatusView status) {
-        this.content.add(status)
+    private void addStatusViews(Map<String, JobWithDetails> jobs){
+        jobs.each { name, job ->
+            println 'Adding job ' + name
+
+            def view = new JobStatusView(job)
+            statuses.add(view)
+            this.content.add(view)
+        }
+
     }
 
     void refresh(){
-
+        //TODO
     }
 }
