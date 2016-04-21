875b3cb3ee87c095adb3fd439d4f354f120e48df
Stefan Härtel <haertel.dev@gmail.com>
2016-04-21 21:11:46 +0200

    Adjust layout

--- src/main/groovy/de/eightbitboy/jenot/ui/JobStatusView.groovy
+++ src/main/groovy/de/eightbitboy/jenot/ui/JobStatusView.groovy
@@ -7,22 +7,26 @@ import javax.swing.border.LineBorder
 import java.awt.*
 
 class JobStatusView extends JPanel {
+
+    private JobWithDetails job
+
     JobStatusView(JobWithDetails job) {
-        setLayout(new FlowLayout(FlowLayout.LEFT))
+        this.job = job
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS))
         setBorder(new LineBorder(Color.RED))
 
-        setUpTop(job)
-        setUpBottom(job)
+        setUpTop()
+        setUpBottom()
     }
 
-    private setUpTop(JobWithDetails job) {
+    private setUpTop() {
         JPanel top = new JPanel(new GridLayout(1, 1))
         top.setBorder(new LineBorder(Color.GREEN))
-        top.add(new JLabel(job.getName()))
+        top.add(new JLabel(this.job.getName()))
         add(top)
     }
 
-    private setUpBottom(JobWithDetails job) {
+    private setUpBottom() {
         JPanel bottom = new JPanel(new GridLayout(1, 1))
         bottom.setBorder(new LineBorder(Color.BLUE))
         bottom.add(new JLabel('Progress'))
