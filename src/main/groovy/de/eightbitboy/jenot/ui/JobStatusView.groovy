bd43a916828803646f19dcd6e71199130f1b36a9
Stefan Härtel <haertel.dev@gmail.com>
2016-04-22 10:36:46 +0200

    Add some colors

--- src/main/groovy/de/eightbitboy/jenot/ui/JobStatusView.groovy
+++ src/main/groovy/de/eightbitboy/jenot/ui/JobStatusView.groovy
@@ -12,6 +12,9 @@ class JobStatusView extends JPanel {
 
     private Job job
 
+    private JPanel top
+    private JPanel bottom
+
     private JLabel jobNameLabel
     private JLabel buildStatusLabel
     private JLabel buildNumberLabel
@@ -27,27 +30,29 @@ class JobStatusView extends JPanel {
     }
 
     private setUpTop() {
-        JPanel top = new JPanel(new GridLayout(1, 3))
+        this.top = new JPanel(new GridLayout(1, 3))
+        this.top.setOpaque(true)
 
         this.jobNameLabel = new JLabel(job.getName())
         this.buildStatusLabel = new JLabel()
         this.buildNumberLabel = new JLabel()
 
-        top.add(this.jobNameLabel)
-        top.add(this.buildStatusLabel)
-        top.add(this.buildNumberLabel)
+        this.top.add(this.jobNameLabel)
+        this.top.add(this.buildStatusLabel)
+        this.top.add(this.buildNumberLabel)
 
-        add(top)
+        add(this.top)
     }
 
     private setUpBottom() {
-        JPanel bottom = new JPanel(new GridLayout(1, 1))
+        this.bottom = new JPanel(new GridLayout(1, 1))
+        this.bottom.setOpaque(true)
 
         this.progressBar = new JProgressBar()
 
-        bottom.add(this.progressBar)
+        this.bottom.add(this.progressBar)
 
-        add(bottom)
+        add(this.bottom)
     }
 
     void refresh() {
@@ -59,8 +64,19 @@ class JobStatusView extends JPanel {
 
         if (buildDetails.isBuilding()) {
             this.buildStatusLabel.setText('BUILDING')
+            this.bottom.setBackground(Color.GRAY)
         } else {
-            this.buildStatusLabel.setText(buildDetails.getResult() as String)
+            String result = buildDetails.getResult() as String
+            this.buildStatusLabel.setText(result)
+
+            if (result == 'SUCCESS') {
+                this.top.setBackground(Color.GREEN)
+                this.bottom.setBackground(Color.GREEN)
+            }
+            if (result == 'FAILURE') {
+                this.top.setBackground(Color.RED)
+                this.bottom.setBackground(Color.RED)
+            }
         }
 
         if (buildDetails.isBuilding()) {
