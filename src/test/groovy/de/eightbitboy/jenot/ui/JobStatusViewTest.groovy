40701bb6ccc74baaf73d0c500ac83c59dc060be7
Stefan Härtel <haertel.dev@gmail.com>
2016-04-24 21:08:11 +0200

    Add simple spock test

--- src/test/groovy/de/eightbitboy/jenot/ui/JobStatusViewTest.groovy
+++ src/test/groovy/de/eightbitboy/jenot/ui/JobStatusViewTest.groovy
@@ -1,4 +1,14 @@
 package de.eightbitboy.jenot.ui
 
-public class JobStatusViewTest {
+import spock.lang.Specification
+
+public class JobStatusViewTest extends Specification {
+	def "this test should work"() {
+		setup:
+		def a = "foo"
+		def b = "bar"
+
+		expect:
+		a + b == "foobar"
+	}
 }
